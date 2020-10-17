d2b4e07a4b6f32066b4fa22753993b2c0e4aabb8
dissipar <vitormonaliza@gmail.com>
2020-10-17 14:30:35 -0300

    testando nova arquitetura

--- api/src/controllers/maravilhosas-controller.js
+++ api/src/controllers/maravilhosas-controller.js
@@ -1,11 +1,58 @@
+const models = require('../models/maravilhosas-models')
+
+
 //Nomes dos métodos para implementação:
 
-//getMaravilhosas
 
-//getMaravilhosaById
+// GET Maravilhosas
+const getMaravilhosas = (req, res) => {
+    const result = models.selectAllData()
+        res.status(200).send(result)
+}
 
-//addMaravilhosa 
 
-//updateMaravilhosa 
+// GET Maravilhosas by Id
 
-//deleteMaravilhosa
+const getMaravilhosaById = (req, res) => {
+    const id = req.params.id
+    const result = models.selectDataById(id)
+    res.status(200).send(result)
+
+}
+
+// POST
+const addMaravilhosa = (req, res) => {
+    const body = req.body
+    const result = models.insertData(body)
+    res.status(201).send('Dado criado com sucesso!')
+    res.status(201).send(result)
+
+}
+
+
+// PUT 
+const updateMaravilhosa = (req, res) => {
+    const id = req.params.id
+    const body = req.body
+    const result = models.updateData(id, body)
+
+    res.status(200).send(result)
+}
+
+// DELETE
+const deleteMaravilhosa = (req, res) => {
+    const id = req.params.id
+    const result = models.deleteData(id)
+    res.status(202).send('Dado deletado com sucesso!')
+
+}
+
+
+
+module.exports = {
+    getMaravilhosas,
+    getMaravilhosaById,
+    addMaravilhosa,
+    updateMaravilhosa,
+    deleteMaravilhosa
+}
